6ff93136b24a69b93f3ade94acf9cf944e23cbb9
Ben Kawalec <bkawalec10@gmail.com>
2022-10-22 19:02:38 -0400

    Server.js / connection.js setup

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
- 
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+  app.use(session(sess));
+
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.use(routes);
+
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
