6f1b21eae05d62df32a08e8598e91671869fb464
Ben Kawalec <bkawalec10@gmail.com>
2022-10-22 22:54:45 -0400

    working tech blog

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Challenge-14
+# Challenge-14 - Tech Blog
+
+This is a site that allows you to create an account and write blog posts. It saves your data so when you come back later you can re-login and view all of your posts.
+
+## Deployed Link 
+- 
+
+
+## Technologies used
+- Express handlebars
+- MySQL
+- Sequelize
+- Bcrypt
+
+
+## Screenshot
+
+
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const app = express();
+const hbs = exphbs.create({ helpers });
+const PORT = process.env.PORT || 3001;
 
 const sess = {
     secret: 'Secret',
